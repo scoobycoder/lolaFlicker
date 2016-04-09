6b3a5a8376cf47f427c427d4dda4c227702778b5
scoobycoder <scoobycoder@gmail.com>
2016-04-09 11:36:14 -0400

    first failing test with GemFile

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rake'
+gem 'rest-client', '1.8.0'
+gem 'rspec', '3.4.0'
---
+++ test_api_key_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+
+describe 'API' do
+
+  it 'should connect to flikr with api key' do
+    expect(false).to eq(true)
+  end
+end
